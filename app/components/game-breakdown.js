fdf5596b967d311edfaff4a088c0e149201b5d50
bradleyden <bleyden92@gmail.com>
2017-08-28 21:33:38 -0400

    Delete Plate Appearance feature works
    Plate appearances can now be deleted with the delete button next to
    its name.
    
    Final step for CRUD will be to link to an edit form when the user
    clicks on the plate appearance.

--- app/components/game-breakdown.js
+++ app/components/game-breakdown.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
     createPlateappearance (plateappearance) {
       plateappearance.game = this.get('game')
       return this.sendAction('createPlateappearance', plateappearance)
+    },
+    deletePlateappearance (plateappearance) {
+      console.log(plateappearance)
+      return this.sendAction('deletePlateappearance', plateappearance)
     }
   }
 });
--- app/components/plateappearance-box.js
+++ app/components/plateappearance-box.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deletePlateappearance () {
+      return this.sendAction('deletePlateappearance', this.get('plateappearance'))
+    }
+  }
 });
