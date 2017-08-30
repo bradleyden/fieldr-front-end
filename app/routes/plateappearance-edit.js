d58d55e0317667b6c4f241975ef574fcade0aa4d
bradleyden <bleyden92@gmail.com>
2017-08-30 00:57:52 -0400

    Dynamic Box Score started
    Started a dynamic box score feature that updates automatically as a
    user adds plate appearances to the game.
    
    Commit contains files for the box score component and logic added
    to the game and PA-edit routes to calculate the score and update
    the box score whenever a PA is created, destroyed or updated.

--- app/routes/plateappearance-edit.js
+++ app/routes/plateappearance-edit.js
@@ -2,12 +2,47 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model (params) {
-    return this.get('store').findRecord('plateappearance', params.plateappearance_id);
+    return this.get('store').findRecord('plateappearance', params.plateappearance_id)
   },
   actions: {
+    // updateScores(pa) {
+    //   const game = pa
+    //   console.log(game)
+    //   let awayRuns = 0
+    //   let homeRuns = 0
+    //   let awayHits = 0
+    //   let homeHits = 0
+    //   let awayErrors = 0
+    //   let homeErrors = 0
+    //   const plateappearanceArray = game.get('plateappearances').toArray()
+    //   plateappearanceArray.forEach(function(item) {
+    //     if (item.data.inninghalf === 'TOP') {
+    //       awayRuns += item.data.runs
+    //       awayErrors += item.data.def_error
+    //       if (item.data.outcome === 'Hit') {
+    //         awayHits += 1
+    //       }
+    //     } else if (item.data.inninghalf === 'BOT'){
+    //       homeRuns += item.data.runs
+    //       homeErrors += item.data.def_error
+    //       if (item.data.outcome === 'Hit') {
+    //         homeHits += 1
+    //       }
+    //     }
+    //   })
+    //   game.set('awayRuns', awayRuns)
+    //   game.set('homeRuns', homeRuns)
+    //   game.set('awayHits', awayHits)
+    //   game.set('homeHits', homeHits)
+    //   game.set('awayErrors', awayErrors)
+    //   game.set('homeErrors', homeErrors)
+    // },
     editPlateappearance(plateappearance) {
-      const game = this
+      const scope = this
       plateappearance.save()
+      // Ember.run.later((function(pa) {
+      //   scope.send('updateScores', pa)
+      // }), 500)
     }
   }
 });
