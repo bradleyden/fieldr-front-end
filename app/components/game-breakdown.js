44079607ce4562582d0ed0c370b4b567975dc8f3
bradleyden <bleyden92@gmail.com>
2017-09-11 23:44:16 -0400

    add sorting settings to pa so that PA icons stay in correct order
    added settings to the game breakdown component to specify PAs be sorted
    by ID so that the order is set in stone and isn't changed when a PA is
    updated.

--- app/components/game-breakdown.js
+++ app/components/game-breakdown.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedPlateappearances: Ember.computed.sort('game.plateappearances', 'sortDefinition'),
+  sortDefinition: ['id'],
   actions: {
     createPlateappearance (plateappearance) {
+      console.log(plateappearance.id)
       plateappearance.game = this.get('game')
       return this.sendAction('createPlateappearance', plateappearance)
     },
